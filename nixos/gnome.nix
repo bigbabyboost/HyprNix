58c9e7dc4a866a09d50edfcf5fc60b2daaee5c53
bigbabyboost <ah0199004@gmail.com>
2023-09-08 18:46:08 +0700

    personal changes

---
+++ nixos/gnome.nix
@@ -0,0 +1,33 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ pkgs, ... }:
+
+{
+  # Enable the X11 windowing system.
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = [ "intel" ];
+      deviceSection = ''
+        Option "DRI" "3"
+        Option "TearFree" "true"
+      '';
+      desktopManager = {
+        gnome.enable = true;
+      };
+      displayManager = {
+        gdm.enable = true;
+        autoLogin.enable = false;
+        sessionPackages = [ pkgs.hyprland ];
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+  ];
+}
+
